aa2c8e58e82097bfd1555acba835ca9d2796d56e
Kelilopz <kelilopz2171804@gmail.com>
2024-05-23 10:38:08 -0500

    feat: :sparkles: Se inicia a consumir la appi
    se añade el url

--- src/modules/musicData.js
+++ src/modules/musicData.js
@@ -1,11 +1,11 @@
 const url = 'https://spotify23.p.rapidapi.com/recommendations/?limit=20&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry';
 const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'e6cf14c6famsh4d970da491ed55cp1625a6jsnadb2f9835b47',
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-        }
-    };
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd87694ba5amsh04d689576cc9c38p1ca7e4jsna778d15fe1ae',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
 let result
 try {
     const response = await fetch(url, options);
@@ -19,10 +19,4 @@ export const sortSongsForPopularity = () => {
     const sortedSongs = data.tracks
     sortedSongs.sort((a, b) => b.popularity - a.popularity)
     return sortedSongs
-}
-
-export const releaseYear = (data) => {
-    const releaseDateFull = data.album.release_date
-    const year = new Date(releaseDateFull).getFullYear()
-    return year
 }
